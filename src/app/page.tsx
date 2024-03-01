bec7bf80faf8bf090406b6b1ef39a07d27b8fbd3
Nielleeeee <jandnllplz@gmail.com>
2024-03-02 02:50:29 +0800

    refactor: added animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import Bored from "@/components/bored";
-import Image from "next/image";
+import { TransitionFadeIn } from "@/components/animation/transition";
 
 export default function Home() {
-
   return (
     <main className="bg-gray-700 flex min-h-screen flex-col justify-center items-center p-24 gap-4">
-      <h1 className="text-white text-2xl font-semibold">U bored?</h1>
+      <TransitionFadeIn>
+        <h1 className="text-white text-2xl font-semibold">U bored?</h1>
+      </TransitionFadeIn>
 
       <Bored />
     </main>
--- src/components/bored/index.tsx
+++ src/components/bored/index.tsx
@@ -6,6 +6,7 @@ import Whitebored from "@/components/whitebored";
 import FormModal from "../modal/formModal";
 import { useFormik } from "formik";
 import * as Yup from "yup";
+import { TransitionMoveUp } from "@/components/animation/transition";
 
 export default function Bored() {
   const [boredData, setBoredData] = useState<BoredTypes | null>(null);
@@ -22,6 +23,7 @@ export default function Bored() {
     }
 
     setBoredData(response);
+    console.log(boredData);
     setIsFetching(false);
   };
 
@@ -51,112 +53,122 @@ export default function Bored() {
   });
 
   return (
-    <section className="flex flex-col justify-center items-center w-full gap-10">
-      <div className="flex gap-4">
-        <button
-          onClick={getNewBored}
-          disabled={isFetching}
-          className="w-max py-3 px-4 bg-blue-500 disabled:bg-blue-800 rounded-lg text-white"
-        >
-          Try this!
-        </button>
+    <TransitionMoveUp>
+      <section className="flex flex-col justify-center items-center w-full gap-10">
+        <div className="flex gap-4">
+          <button
+            onClick={getNewBored}
+            disabled={isFetching}
+            className="w-max py-3 px-4 bg-blue-500 disabled:bg-blue-800 rounded-lg text-white"
+          >
+            Try this!
+          </button>
 
-        <FormModal
-          modalTitle={"Customize ur query"}
-          buttonTitle={"Or this!"}
-          isOpen={isOpenModal}
-          setIsOpen={setIsOpenModal}
-        >
-          <form onSubmit={formik.handleSubmit} className="flex flex-col gap-4">
-            <div>
-              <label className="sr-only" htmlFor="type">
-                Type
-              </label>
-              <select
-                name="type"
-                id="type"
-                onChange={formik.handleChange}
-                value={formik.values.type}
-                className={`w-full rounded-lg p-3 text-sm ${
-                  formik.touched.type && formik.errors.type
-                    ? "border-red-400 border-2"
-                    : "border-gray-400 border"
-                }`}
-              >
-                <option value="" defaultChecked disabled className="text-gray-400">
-                  Activity Type
-                </option>
-                <option value="education">Education</option>
-                <option value="recreational">Recreational</option>
-                <option value="social">Social</option>
-                <option value="diy">DIY</option>
-                <option value="charity">charity</option>
-                <option value="cooking">Cooking</option>
-                <option value="relaxation">relaxation</option>
-                <option value="music">Music</option>
-                <option value="busywork">Busywork</option>
-              </select>
-            </div>
+          <FormModal
+            modalTitle={"Customize ur query"}
+            buttonTitle={"Or this!"}
+            isOpen={isOpenModal}
+            setIsOpen={setIsOpenModal}
+          >
+            <form
+              onSubmit={formik.handleSubmit}
+              className="flex flex-col gap-4"
+            >
+              <div>
+                <label className="sr-only" htmlFor="type">
+                  Type
+                </label>
+                <select
+                  name="type"
+                  id="type"
+                  onChange={formik.handleChange}
+                  value={formik.values.type}
+                  className={`w-full rounded-lg p-3 text-sm ${
+                    formik.touched.type && formik.errors.type
+                      ? "border-red-400 border-2"
+                      : "border-gray-400 border"
+                  }`}
+                >
+                  <option
+                    value=""
+                    defaultChecked
+                    disabled
+                    className="text-gray-400"
+                  >
+                    Activity Type
+                  </option>
+                  <option value="education">Education</option>
+                  <option value="recreational">Recreational</option>
+                  <option value="social">Social</option>
+                  <option value="diy">DIY</option>
+                  <option value="charity">Charity</option>
+                  <option value="cooking">Cooking</option>
+                  <option value="relaxation">Relaxation</option>
+                  <option value="music">Music</option>
+                  <option value="busywork">Busywork</option>
+                </select>
+              </div>
 
-            <div>
-              <label className="sr-only" htmlFor="participants">
-                Participants
-              </label>
-              <input
-                className="w-full rounded-lg border-gray-400 border p-3 text-sm"
-                placeholder="Participants"
-                type="number"
-                id="participants"
-                name="participants"
-                onChange={formik.handleChange}
-                value={formik.values.participants}
-              />
-            </div>
+              <div>
+                <label className="sr-only" htmlFor="participants">
+                  Participants
+                </label>
+                <input
+                  className="w-full rounded-lg border-gray-400 border p-3 text-sm"
+                  placeholder="Participants"
+                  type="number"
+                  id="participants"
+                  name="participants"
+                  onChange={formik.handleChange}
+                  value={formik.values.participants}
+                />
+              </div>
 
-            <div>
-              <label className="sr-only" htmlFor="price">
-                Price
-              </label>
-              <input
-                className="w-full rounded-lg border-gray-400 border p-3 text-sm"
-                placeholder="Price"
-                type="number"
-                id="price"
-                name="price"
-                onChange={formik.handleChange}
-                value={formik.values.price}
-              />
-            </div>
+              <div>
+                <label className="sr-only" htmlFor="price">
+                  Price
+                </label>
+                <input
+                  className="w-full rounded-lg border-gray-400 border p-3 text-sm"
+                  placeholder="Price"
+                  type="number"
+                  id="price"
+                  name="price"
+                  onChange={formik.handleChange}
+                  value={formik.values.price}
+                />
+              </div>
 
-            <div>
-              <label className="sr-only" htmlFor="accessibility">
-                Accessibility
-              </label>
-              <input
-                className="w-full rounded-lg border-gray-400 border p-3 text-sm"
-                placeholder="Accessibility"
-                type="number"
-                id="accessibility"
-                name="accessibility"
-                onChange={formik.handleChange}
-                value={formik.values.accessibility}
-              />
-            </div>
+              <div>
+                <label className="sr-only" htmlFor="accessibility">
+                  Accessibility
+                </label>
+                <input
+                  className="w-full rounded-lg border-gray-400 border p-3 text-sm"
+                  placeholder="Accessibility"
+                  type="number"
+                  id="accessibility"
+                  name="accessibility"
+                  onChange={formik.handleChange}
+                  value={formik.values.accessibility}
+                />
+              </div>
 
-            <div>
-              <button
-                type="submit"
-                disabled={formik.isSubmitting}
-                className="w-full py-3 px-4 bg-blue-500 disabled:bg-blue-800 rounded-lg text-white"
-              >
-                {formik.isSubmitting ? "Generating..." : "Generate"}
-              </button>
-            </div>
-          </form>
-        </FormModal>
-      </div>
+              <div>
+                <button
+                  type="submit"
+                  disabled={formik.isSubmitting}
+                  className="w-full py-3 px-4 bg-blue-500 disabled:bg-blue-800 rounded-lg text-white"
+                >
+                  {formik.isSubmitting ? "Generating..." : "Generate"}
+                </button>
+              </div>
+            </form>
+          </FormModal>
+        </div>
 
-      {boredData ? <Whitebored {...boredData} /> : null}
-    </section>
+        {boredData ? <Whitebored key={boredData.key} BoredData={boredData} /> : null}
+      </section>
+    </TransitionMoveUp>
   );
 }
